73647b1e9f2d4583e214e6bd6e2a3d27edd424bc
Andrei Ovchinnikov <aovchin@gmail.com>
2021-03-29 05:39:19 +0300

    Added JSON Schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -3,21 +3,23 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
 
 public class MobileBankApiTestV4 {
-//    @Test
-//    void shouldReturnDemoAccounts() {
-//        // Given - When - Then
-//        // Предусловия
-//        given()
-//                .baseUri("http://localhost:9999/api/v1")
-//                // Выполняемые действия
-//                .when()
-//                .get("/demo/accounts")
-//                // Проверки
-//                .then()
-//                .statusCode(200)
-//                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-//        ;
-//    }
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
+    }
 }
